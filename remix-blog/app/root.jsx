f223e9eb225e13a6d22e2e2d2022fe808920bb83
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-05-27 10:54:34 +0000

    clean up, new root component...

--- remix-blog/app/root.jsx
+++ remix-blog/app/root.jsx
@@ -1,31 +1,68 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Outlet, LiveReload, Link, Links, Meta } from '@remix-run/react';
+import globalStylesUrl from './styles/global.css';
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta = () => (
+  {
+    charset: "utf-8",
+    title: "Remix Blog",
+    viewport: "width=device-width,initial-scale=1",
+  }
+)
+
+export const links = () => [{rel: "stylesheet", href: globalStylesUrl}];
 
 export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
+        <title>{title ? title : 'Remix Blog'}</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        {/* Child routes go here */}
+        {children}
+
+        {/* Sets up automatic reload when you change code */}
+        {/* and only does anything during development */}
+        {
+          process.env.NODE_ENV === 'development'
+           ? <LiveReload />
+           : null      
+        }
       </body>
     </html>
-  );
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to='/' className='logo'> 
+          Remix        
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link to='/post'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main className="main">
+        {children}
+      </main>
+    </>
+  )
 }
